b29cfd1acfe0f9f7bfcffb44db2fc34a137feebe
pookiege <pookiege@gmail.com>
2018-11-27 14:04:43 +0100

    Update Nagios_DIP.js

--- Nagios_DIP.js
+++ Nagios_DIP.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Nagios : que le DIP V2
 // @namespace    https://prod.etat-ge.ch/ctipilotage-srv/cgi-bin/status.cgi?host=monitoring_dispo&limit=0
-// @version      1.05
+// @version      1.06
 // @description  Nagios version allégée
 // @author       NTH
 // @require      http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
