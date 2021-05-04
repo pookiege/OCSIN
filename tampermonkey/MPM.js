338a6a1a340094be1f4815b63231a15d33b8ee6f
pookiege <pookiege@gmail.com>
2021-05-04 11:02:20 +0200

    Update MPM.js

--- tampermonkey/MPM.js
+++ tampermonkey/MPM.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       NTH
-// @match        https://prod.etat-ge.ch/projetdessimanager/prestation/financeGL.php*
+// @match        https://prod.etat-ge.ch/projetdessimanager/*
 // @grant        none
 // @updateURL    https://github.com/pookiege/OCSIN/tampermonkey/MPM.js
 // @downloadURL  https://github.com/pookiege/OCSIN/tampermonkey/MPM.js
@@ -12,11 +12,12 @@
 
 (function() {
     'use strict';
-document.getElementsByName('daCmdFac')[0].checked = true;
-document.getElementsByName('da')[0].checked = true;
-document.getElementsByName('com')[0].checked = true;
-document.getElementsByName('fact')[0].checked = true;
-document.getElementsByName('rec')[0].checked = true;
-
-    // Your code here...
+    document.getElementById('skin_content').style.width = "1500px";
+    document.getElementById('skin_contenu').style.width = "1300px";
+    //spécifique finances
+    document.getElementsByName('daCmdFac')[0].checked = true;
+    document.getElementsByName('da')[0].checked = true;
+    document.getElementsByName('com')[0].checked = true;
+    document.getElementsByName('fact')[0].checked = true;
+    document.getElementsByName('rec')[0].checked = true;
 })();
