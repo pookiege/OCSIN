c746e4f615d0f0cdea41486af9ba659671c32d1f
pookiege <pookiege@gmail.com>
2021-05-19 08:10:37 +0200

    Update Nagios_DIP.js

--- JS/Nagios_DIP.js
+++ JS/Nagios_DIP.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Nagios : que le DIP
 // @namespace    https://prod.etat-ge.ch/ctipilotage-srv/cgi-bin/status.cgi?host=monitoring_dispo&limit=0
-// @version      1.15
+// @version      1.16
 // @description  Nagios version allégée
 // @author       NTH
 // @require      http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
@@ -13,14 +13,14 @@
 // @downloadURL  https://raw.githubusercontent.com/pookiege/OCSIN/master/Nagios_DIP.js
 // ==/UserScript==
 
-(function() {
-    GM_addStyle('.statusOK       { font-size: 12pt; }');
-    GM_addStyle('.statusWARNING  { font-size: 14pt; }');
-    GM_addStyle('.statusCRITICAL { font-size: 14pt; background-color: #ff6666; font-weight: bold;}');
-    GM_addStyle('.statusCRITICAL p { font-size: 8pt; background-color: #ff6666; }');
-    GM_addStyle('.titre { font-size: 20pt; background-color: #0000CD; color : #ffffff; border: 1px solid #777777; text-align: center; padding: 0 5 0 5; }');
+( function() {
+    GM_addStyle('.statusOK       { font-size: 11pt; }');
+    GM_addStyle('.statusWARNING  { font-size: 12pt; }');
+    GM_addStyle('.statusCRITICAL { font-size: 12pt; background-color: #ff6666; font-weight: bold;}');
+    GM_addStyle('.statusCRITICAL p { font-size: 6pt; background-color: #ff6666; }');
+    GM_addStyle('.titre { font-size: 16pt; background-color: #0000CD; color : #ffffff; border: 1px solid #777777; text-align: center; padding: 0 5 0 5; }');
     GM_addStyle('.tableRes { margin:10px; width : 100%}');
     GM_addStyle('.preview { float:right; height: 15px}');
-    purgeNagios(dip);
+    purgeNagios(getDip());
 })
 ();
